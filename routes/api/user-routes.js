9967bb61c4fa2fa3146bf06ec236c803a6287ad6
vhivestate <garcia.valeria001@gmail.com>
2022-07-24 22:55:58 -0600

    conrollers

--- routes/api/user-routes.js
+++ routes/api/user-routes.js
@@ -1,6 +1,37 @@
+const router = require('express').Router();
+
+const {
+    getAllPizza,
+    getPizzaById,
+    createPizza,
+    updatePizza,
+    deletePizza
+  } = require('../../controllers/pizza-controller');
+
+// Set up GET all and POST at /api/pizzas
+router
+  .route('/')
+  .get(getAllPizza)
+  .post(createPizza);
+
+// Set up GET one, PUT, and DELETE at /api/pizzas/:id
+router
+router
+  .route('/:id')
+  .get(getPizzaById)
+  .put(updatePizza)
+  .delete(deletePizza);
+
+module.exports = router;
+
+
+
+
+
 /api/users
 // GET all users
 
+  
 // GET a single user by its _id and populated thought and friend data
 
 // POST a new user
